51f189bfe8b520b509a433acded378288dd9e938
tallman <vedunaik@gmail.com>
2023-09-27 21:47:11 +0530

    removed nextui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-import {nextui} from "@nextui-org/react";
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}","./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       primary: 'Orbitron',
@@ -32,7 +31,6 @@ export default {
       },
     },
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 }
 
