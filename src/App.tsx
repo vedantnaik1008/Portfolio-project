6aad483d119ed97ba87c3f503094c8e744da0c66
tallman <vedunaik@gmail.com>
2023-10-30 12:03:06 +0530

    added framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Header from './Components/Header'
 import Skills from './Components/Skills'
 import Work from './Components/Work'
 
-
 const App = () => {
   return (
     <div className="bg-[#131313]  overflow-hidden">
