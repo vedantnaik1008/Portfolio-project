abdacfd4cd4bb3fb3b7ec8828d78468080b376f7
tallman <vedunaik@gmail.com>
2023-11-01 14:20:58 +0530

    added aria-label

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -69,7 +69,7 @@ const Contact = () => {
                 {isSubmitting ? 'Submitting...' : 'Submit'}
               </button>
               <span className='text-white flex items-center justify-center font-semibold'>Or Dm On</span>
-              <a href="https://twitter.com/vedantnaik108" className='btn-tweet hover:scale-105 bg-[#FFFFFF] shadow-[inset_0_-4px_0_0_rgba(0,0,0,.25)]  btn-lg  transition-all duration-300 ease-out flex items-center justify-center '>
+              <a href="https://twitter.com/vedantnaik108" className='btn-tweet hover:scale-105 bg-[#FFFFFF] shadow-[inset_0_-4px_0_0_rgba(0,0,0,.25)]  btn-lg  transition-all duration-300 ease-out flex items-center justify-center' aria-label="Twitter">
               <svg fill="currentColor" viewBox="0 0 24 24" className="h-5 w-5" aria-hidden="true"><path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"></path></svg>
               </a>
             </div>
