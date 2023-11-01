5013092f3d1f08d0a761289c7082c69e4f697be4
tallman <vedunaik@gmail.com>
2023-11-01 14:35:05 +0530

    added sizes attribute

--- src/Components/Work.tsx
+++ src/Components/Work.tsx
@@ -22,7 +22,7 @@ const Work = () => {
           {projectData.map((project) => (
             <div key={project.id} className="group relative overflow-hidden border-[2px] border-[#5a5a5a] rounded-xl mt-6">
               <div onClick={() => setIsClicked(project.id)} className="group-hover:bg-black/70 w-full h-full absolute z-30 transition-all duration-100"></div>
-                <img loading="lazy" width="576px" height="400px" className='group-hover:scale-125 transition-all duration-500 w-[100%] h-[400px] object-cover' src={project.image} alt="project-img" />
+                <img loading="lazy" width="576px" height="400px" sizes='(min-width: 960px) 45vw, 90vw' className='group-hover:scale-125 transition-all duration-500 w-[100%] h-[400px] object-cover' src={project.image} alt="project-img" />
               <div className="absolute -top-[50%] left-[40%] group-hover:top-[50%] transition-all duration-500 z-40">
                   <span onClick={() => setIsClicked(project.id)} className="text-3xl text-white text-gradient">Click</span>
               </div>
